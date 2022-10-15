424e8e490c6dc470c76fd0702fc6d21bedaf4067
abba_ye38 <abba_ye38@STHQ012B-03>
2022-10-15 17:40:01 +0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    class Originator
+    {
+        // For the sake of simplicity, the originator's state is stored inside a
+        // single variable.
+        private string _state;
+
+        public Originator(string state)
+        {
+            this._state = state;
+            Console.WriteLine("Originator: My initial state is: " + state);
+        }
+
+        // The Originator's business logic may affect its internal state.
+        // Therefore, the client should backup the state before launching
+        // methods of the business logic via the save() method.
+        public void DoSomething()
+        {
+            Console.WriteLine("Originator: I'm doing something important.");
+            this._state = this.GenerateRandomString(30);
+            Console.WriteLine($"Originator: and my state has changed to: {_state}");
+        }
+
+        private string GenerateRandomString(int length = 10)
+        {
+            string allowedSymbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string result = string.Empty;
+
+            while (length > 0)
+            {
+                result += allowedSymbols[new Random().Next(0, allowedSymbols.Length)];
+
+                Thread.Sleep(12);
+
+                length--;
+            }
+
+            return result;
+        }
+
+        // Saves the current state inside a memento.
+        public IMemento Save()
+        {
+            return new ConcreteMemento(this._state);
+        }
+
+        // Restores the Originator's state from a memento object.
+        public void Restore(IMemento memento)
+        {
+            if (!(memento is ConcreteMemento))
+            {
+                throw new Exception("Unknown memento class " + memento.ToString());
+            }
+
+            this._state = memento.GetState();
+            Console.Write($"Originator: My state has changed to: {_state}");
+        }
+    }
+
+
+    public interface IMemento
+    {
+        string GetName();
+
+        string GetState();
+
+        DateTime GetDate();
+    }
+
+
+    class ConcreteMemento : IMemento
+    {
+        private string _state;
+
+        private DateTime _date;
+
+        public ConcreteMemento(string state)
+        {
+            this._state = state;
+            this._date = DateTime.Now;
+        }
+
+        // The Originator uses this method when restoring its state.
+        public string GetState()
+        {
+            return this._state;
+        }
+
+        // The rest of the methods are used by the Caretaker to display
+        // metadata.
+        public string GetName()
+        {
+            return $"{this._date} / ({this._state.Substring(0, 9)})...";
+        }
+
+        public DateTime GetDate()
+        {
+            return this._date;
+        }
+    }
+
+    class Caretaker
+    {
+        private List<IMemento> _mementos = new List<IMemento>();
+
+        private Originator _originator = null;
+
+        public Caretaker(Originator originator)
+        {
+            this._originator = originator;
+        }
+
+        public void Backup()
+        {
+            Console.WriteLine("\nCaretaker: Saving Originator's state...");
+            this._mementos.Add(this._originator.Save());
+        }
+
+        public void Undo()
+        {
+            if (_mementos.Count == 0)
+            {
+                return;
+            }
+
+            //var memento = _mementos.Last();
+            //this._mementos.Remove(memento);
+            var memento = _mementos[Index];
+            Index--;
+            Console.WriteLine("Care Taker :  Restoring state" + memento.GetName());
+
+            try
+            {
+                _originator.Restore(memento);
+            }
+            catch (Exception)
+            {
+                this.Undo();
+            }
+
+        }
+        public void Redo()
+        {
+            var memento = _mementos[Index];
+            Index++;
+            Console.WriteLine("Care Taker :  Restoring state" + memento.GetName());
+        }
+        public int Index { get; set; } = -1;
+        public void BackUp()
+        {
+            Console.WriteLine("\nCareTaker Saving Originator state . . . ");
+            this._mementos.Add(_originator.Save());
+            Index++;
+        }
+
+
+        public void ShowHistory()
+        {
+            Console.WriteLine("Caretaker: Here's the list of mementos:");
+
+            foreach (var memento in this._mementos)
+            {
+                Console.WriteLine(memento.GetName());
+            }
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Originator originator = new Originator("Super duper super puper super");
+            var careTaker = new CareTaker(originator);
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Bitmap captureBitmap = new Bitmap(1024, 1024, PixelFormat.Format32bppArgb);
+
+            System.Drawing.Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+
+            Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+
+            captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+            //Saving the Image File (I am here Saving it in My E drive).
+
+            string guidGen = Guid.NewGuid().ToString();
+            string imgPath = $"~..\\..\\{ guidGen}.jpg";
+            captureBitmap.Save($@"{imgPath}", ImageFormat.Jpeg);
+
+            //System.Windows.MessageBox.Show($"{captureBitma}");
+
+        }
+        
+    }
+}
